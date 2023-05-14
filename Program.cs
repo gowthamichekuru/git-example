a50009e8771403e38f3d1ca6af8add85e69049be
gowthamichekuru <133589182+gowthamichekuru@users.noreply.github.com>
2023-05-14 17:17:21 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, teams!");
+            Console.WriteLine("Hello, team digital scripts!");
         }
     }
 }
